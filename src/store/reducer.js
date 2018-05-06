8ed60a49dcbb0fb6d9d439931e829c3373dde10b
Carlos Aguilera <a.carlosing@gmail.com>
2018-05-06 09:43:09 -0500

    Installing redux and react redux.

---
+++ src/store/reducer.js
@@ -0,0 +1,11 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+    ingredientes: [],
+    totalPrice: 4
+};
+
+const redcuer = (state = initialState, actions) => {
+};
+
+export default redcuer;
