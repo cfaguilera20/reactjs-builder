0eba31e87b1628a49f7a50dfac0435cc28a93c55
Carlos Aguilera <a.carlosing@gmail.com>
2018-05-18 11:49:29 -0500

    Resetting the price after purchases.

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -46,6 +46,7 @@ const redcuer = (state = initialState, action) => {
                     cheese: action.ingredients.cheese,
                     meat: action.ingredients.meat
                 },
+                totalPrice: 4,
                 error: false
             };
         case actionTypes.FETCH_INGREDIENTS_FAILED:
