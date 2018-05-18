c54ce6fb005a00a044b97419415dccead3f46e88
Carlos Aguilera <a.carlosing@gmail.com>
2018-05-18 16:25:53 -0500

    Adding an auth form.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,11 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = props => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+        <NavigationItem link="/" exact>
+            Burger Builder
+        </NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
+        <NavigationItem link="/auth">Authenticate</NavigationItem>
     </ul>
 );
 
