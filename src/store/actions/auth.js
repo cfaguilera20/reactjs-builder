9660d8e34d155726ae9f9daad750b9bacd769c6a
Carlos Aguilera <a.carlosing@gmail.com>
2018-05-20 15:01:34 -0500

    Persistent auth state with localStorage.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,10 @@ export const authFail = error => {
 };
 
 export const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationDate");
+    localStorage.removeItem("userId");
+
     return {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -54,6 +58,14 @@ export const auth = (email, password, isSignup) => {
             .post(url, authData)
             .then(response => {
                 console.log(response);
+
+                const expirationDate = new Date(
+                    new Date().getTime() + response.data.expiresIn * 1000
+                );
+                localStorage.setItem("toke", response.data.idToken);
+                localStorage.setItem("expirationDate", expirationDate);
+                localStorage.setItem("userId", response.data.localId);
+
                 dispatch(
                     authSuccess(response.data.idToken, response.data.localId)
                 );
@@ -70,5 +82,30 @@ export const setAuthRedirectPath = path => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(
+                localStorage.getItem("expirationDate")
+            );
+
+            if (expirationDate > new Date()) {
+                const userId = localStorage.getItem("userId");
+                dispatch(authSuccess(token, userId));
+                dispatch(
+                    checkAuthTimeout(
+                        expirationDate.getSeconds() - new Date().getSeconds()
+                    )
+                );
+            } else {
+                dispatch(logout());
+            }
+        }
     };
 };
