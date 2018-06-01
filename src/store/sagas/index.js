0369833b77ce9b8d21319e2c7a60952699d5c434
Carlos Aguilera <a.carlosing@gmail.com>
2018-06-01 09:00:45 -0500

    Handling authentication with a saga.

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,9 +1,10 @@
 import { takeEvery } from "redux-saga/effects";
 
 import * as actionTypes from "../actions/actionTypes";
-import { logoutSaga, checkoutTimeoutSaga } from "./auth";
+import { logoutSaga, checkoutTimeoutSaga, authUserSaga } from "./auth";
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkoutTimeoutSaga);
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
 }
